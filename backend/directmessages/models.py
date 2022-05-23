af3c21d219e4d41a35e20f94ace4019f0b9ffecb
Ennin Jeter <jeterennin@gmail.com>
2022-05-23 16:08:23 -0500

    Added info into the Admin file for both apps and made migrate

--- backend/directmessages/models.py
+++ backend/directmessages/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.dispatch import receiver
 from authentication.models import User
 # Create your models here.
-class Message(models.Model):
+
+
+class DirectMessage(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_received')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
     message = models.CharField(max_length=255)
--- backend/homes/models.py
+++ backend/homes/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Home(models.Model):
-    sale = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     bedroom = models.IntegerField()
     bath = models.IntegerField()
