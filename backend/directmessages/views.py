ae74465fdd96aaddf932a1ee7e71377805bb6812
Ennin Jeter <jeterennin@gmail.com>
2022-05-23 19:18:19 -0500

    Updated views folders in DirectMessage + Homes folder

--- backend/directmessages/views.py
+++ backend/directmessages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response Response
+from rest_framework.permission import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import DirectMessage
+from .serializer import DirectMessageSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
--- backend/homes/views.py
+++ backend/homes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response Response
+from rest_framework.permission import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Home
+from .serializer import HomeSerializer
+from django.contrib.auth.models import User
 # Create your views here.
